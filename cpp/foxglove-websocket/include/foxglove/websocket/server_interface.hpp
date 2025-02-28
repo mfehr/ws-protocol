4d6bf6dee326198cc1f4ba3006e960f684218c6e
Marius Fehr <mbf@voliro.com>
2025-03-01 00:47:56 +0100

    make num threads configurable

--- cpp/foxglove-websocket/include/foxglove/websocket/server_interface.hpp
+++ cpp/foxglove-websocket/include/foxglove/websocket/server_interface.hpp
@@ -51,6 +51,7 @@ struct ServerOptions {
   std::string certfile = "";
   std::string keyfile = "";
   std::string sessionId;
+  uint8_t numWorkerThreads = 1;
   bool useCompression = false;
   std::vector<std::regex> clientTopicWhitelistPatterns;
 };
