abf070e11e9000c1e699c49d3f0cf25a67aa5d9e
Marius Fehr <mbf@voliro.com>
2025-03-01 00:38:20 +0100

    release the threadpool

--- cpp/foxglove-websocket/include/foxglove/websocket/websocket_server.hpp
+++ cpp/foxglove-websocket/include/foxglove/websocket/websocket_server.hpp
@@ -266,7 +266,7 @@ inline Server<ServerConfiguration>::Server(std::string name, LogCallback logger,
   _server.set_listen_backlog(128);
 
   // Callback queue for handling client requests.
-  _handlerCallbackQueue = std::make_unique<CallbackQueue>(_logger, /*numThreads=*/1ul);
+  _handlerCallbackQueue = std::make_unique<CallbackQueue>(_logger, /*numThreads=*/10ul);
 }
 
 template <typename ServerConfiguration>
